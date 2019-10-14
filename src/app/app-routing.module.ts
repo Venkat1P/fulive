a67802aa10b60e8ad2cc77362527887dcc01bea2
venkatsrikanth <venkat.ieee55@gmail.com>
2019-10-14 13:01:14 +0530

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'explore', loadChildren: './explore/explore.module#ExplorePageModule' },
+  { path: 'live', loadChildren: './live/live.module#LivePageModule' },
+  { path: 'saved', loadChildren: './saved/saved.module#SavedPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -8,45 +8,65 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+              import('../home/home.module').then(m => m.HomePageModule)
           }
         ]
       },
       {
-        path: 'tab2',
+        path: 'explore',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+              import('../explore/explore.module').then(m => m.ExplorePageModule)
           }
         ]
       },
       {
-        path: 'tab3',
+        path: 'live',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../live/live.module').then(m => m.LivePageModule)
+          }
+        ]
+      },
+      {
+        path: 'saved',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../saved/saved.module').then(m => m.SavedPageModule)
+          }
+        ]
+      },
+      {
+        path: 'profile',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../profile/profile.module').then(m => m.ProfilePageModule)
           }
         ]
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
